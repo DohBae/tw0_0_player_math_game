056109834f580ffc6db317281e255f7ab426c77f
DohBae <bdoherty92@msn.com>
2022-09-19 21:59:26 +0000

    Add equals statement

--- question.rb
+++ question.rb
@@ -1,17 +1,18 @@
 # Generates two random numbers between 1 and 20 to sum 
 
 class Questions
+  # initialize class with sum = 0
   def initialize ()
     @sum = 0
   end
-  attr_writer :sum
+  attr_reader :sum
   
   # generate two random numbers between 1-20 and add them together
   def random_numbers
-    puts @number1 = rand(1..20)
-    puts @number2 = rand(1..20)
-    puts @sum = @number1 + @number2
-  end
+     @number1 = rand(1..20)
+     @number2 = rand(1..20)
+     @sum = @number1 + @number2
+     puts "What does #{@number1} + #{@number2} equal?"
+    end
 end
 
-Questions.method_defined? :random_numbers
