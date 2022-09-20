4c65b3d5b497ff1649469dcd583183e68321e6ce
DohBae <bdoherty92@msn.com>
2022-09-20 00:04:05 +0000

    Fix question class bugs

--- game.rb
+++ game.rb
@@ -1,41 +1,49 @@
+require './Question'
+require './Player'
+
 class Game
-  def initialize(name)
-    @name = name
-    @player1 = Player.new('Player 1')
-    @player2 = Player.new('Player 2')
+  def initialize(players)
+    @players = players
+    @curr = 0
   end
 
   def start
-    puts "New Game Started. Welcome #{@player1.name} and #{@player2.name}"
+    puts "New game"
     turn
   end
 
-  def show_stats
-    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
-  end
-
-  def winner(player)
+  def win(player)
     puts "#{player.name} wins with the score of #{player.lives}/3"
     puts '---------GAME-OVER--------'
     puts 'Good bye!'
     exit(0)
   end
 
-  def check_score
-    if @player1.is_dead
-      winner(@player2)
-    elsif @player2.is_dead
-      winner(@player1)
+  def turn
+    #players = [player1, player2 ....]
+    # turn = (turn + 1) % players.length
+    # 0 -> (0 + 1) % 
+    while !@players[@curr].is_dead do
+     
+      newQuestion = Question.new
+      newQuestion.print_question(@players[@curr])
+      print '> '
+      @userchoice = $stdin.gets.chomp
+      if newQuestion.answer
+        # if question is wrong than player.minus_life
+        # display message "wrong"
+        # display the scor
+      @curr = (@curr + 1) % @players.length
+
     end
+
+    win(@players[@curr])
+
+      # DISPLAY message #{player} wubs with score of 1/3 
+      # DISPLAY GAME OVER
+    
+
+    # @players[0]
   end
 
-  def turn
-    @player1.new_question
-    check_score
-    @player2.new_question
-    check_score
-    show_stats
-    puts '---------NEW-TURN---------'
-    turn
-  end
 end
